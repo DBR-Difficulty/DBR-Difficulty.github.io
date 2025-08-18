fd00396267a62ab9ef933a22fdfe0668e262e3bb
DBR-Difficulty <dbrdifficulty@gmail.com>
2025-08-19 05:21:21 +0900

    新規☆6以下対応

--- convert_alllog_DBR.py
+++ convert_alllog_DBR.py
@@ -117,6 +117,22 @@ level_ignore_exceptions = [
     ("Blame", "N"),
     ("EDEN", "N"),
     ("e-motion 2003 -romantic extra-", "N"),
+    ("V", "N"),
+    ("Ganymede", "N"),
+    ("neu", "N"),
+    ("Übertreffen", "N"),
+    ("CaptivAte～浄化～", "N"),
+    ("405nm (Ryu☆mix)", "N"),
+    ("こっちを向いてよ", "H"),
+    ("こっちを向いてよ", "N"),
+    ("quasar", "N"),
+    ("Skyscraper", "H"),
+    ("Summer Vacation(CU mix)", "N"),
+    ("Voltage (feat. Hidemaru)", "N"),
+    ("earth scape", "H"),
+    ("LOVE IS ORANGE", "H"),
+    ("Fly Away To India", "H"),
+    ("GRADIUSIC CYBER", "H"),
 ]
 
 def convert_alllog(*args):
