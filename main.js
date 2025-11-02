0a42c068c173496ce3509cbad02cdcb28acf8332
DBR-Difficulty <dbrdifficulty@gmail.com>
2025-11-02 19:34:28 +0900

    IRtest

---
+++ main.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDgU5OwLsDBoTeq_jXS-frSAf9kDDwl6L0",
+  authDomain: "dbr-ir.firebaseapp.com",
+  projectId: "dbr-ir",
+  storageBucket: "dbr-ir.firebasestorage.app",
+  messagingSenderId: "464886830974",
+  appId: "1:464886830974:web:f90d3605f62673136f052f",
+  measurementId: "G-THHFG18D5G"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
